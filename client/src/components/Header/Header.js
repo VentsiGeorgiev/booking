0819f9b74aeb1e403767f31e0cee77bfff568656
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-11-04 11:01:25 +0100

    Add desktop navigaiton

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,38 @@
 import { BiUserCircle, BiMenu } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 
 function Header() {
+
+    const { width } = useWindowDimensions();
+
+    const mobileNavigation = (
+        <div className={styles.header__icons}>
+            <BiUserCircle />
+            <BiMenu />
+        </div>
+    );
+
+    const desktopNavigation = (
+        <ul className={styles.header__links}>
+            <li className={styles.header__link}>
+                <Link className='btn btn-secondary' to='/register'>Register</Link>
+            </li>
+            <li className={styles.header__link}>
+                <Link className='btn btn-secondary' to='/sign-in'>sign-in</Link>
+            </li>
+        </ul>
+    );
+
     return (
         <header className={styles.header}>
             <section className={`container ${styles.header__wrapper}`}>
                 <div className={styles.header__logo}>Booking.com</div>
-                <div className={styles.header__icons}>
-                    <BiUserCircle />
-                    <BiMenu />
-                </div>
+                {width <= 640
+                    ? mobileNavigation
+                    : desktopNavigation
+                }
             </section>
         </header>
     );
